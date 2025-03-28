9ebd5a96559aa895711c56375466a4e6f2ae5259
ERMR1 <114266959+ermr2@users.noreply.github.com>
2025-03-28 17:53:43 -0600

    Segunda interface usuario y contrasena ademas de botones ingresar

---
+++ app/src/main/java/com/example/minsupgest/Iniciosessionus.kt
@@ -0,0 +1,20 @@
+package com.example.minsupgest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Iniciosessionus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_iniciosessionus)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
