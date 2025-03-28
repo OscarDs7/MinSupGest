4f7cee5e250de319f7c1b47cd07d34918687a444
OscarDs7 <a22110112@ceti.mx>
2025-03-27 20:39:22 -0600

    Frontend del activity_registrar_usuario.xml

---
+++ app/src/main/java/com/example/minsupgest/RegistrarUsuarioActivity.kt
@@ -0,0 +1,16 @@
+package com.example.minsupgest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistrarUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registrar_usuario)
+    }
+
+}
