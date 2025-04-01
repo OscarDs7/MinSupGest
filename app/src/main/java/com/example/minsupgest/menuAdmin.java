ae8e92c92dd8fbd62e18fa3a03dad0fc5564a56f
OscarDs7 <a22110112@ceti.mx>
2025-03-31 23:58:48 -0600

    Implementación de Menús: Admin y Empleados

---
+++ app/src/main/java/com/example/minsupgest/menuAdmin.java
@@ -0,0 +1,6 @@
+package com.example.minsupgest;
+
+import android.app.Activity;
+
+public class menuAdmin extends Activity {
+}
---
+++ app/src/main/java/com/example/minsupgest/menuEmpleados.java
@@ -0,0 +1,6 @@
+package com.example.minsupgest;
+
+import android.app.Activity;
+
+public class menuEmpleados extends Activity {
+}
