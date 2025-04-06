ff6176f466ba1bcb05a5e44edc3e93391a691d8e
ERMR1 <114266959+ermr2@users.noreply.github.com>
2025-04-05 19:12:36 -0600

    Ultima interface de inicio de session

--- app/src/main/java/com/example/minsupgest/MenuAdminActivity.kt
+++ app/src/main/java/com/example/minsupgest/MenuAdminActivity.kt
@@ -1,13 +1,26 @@
 package com.example.minsupgest
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.FirebaseDatabase
 
 class MenuAdminActivity : AppCompatActivity() {
+    private val db = FirebaseDatabase.getInstance()
+    val ref = db.getReference("productos")
+    private lateinit var guardar: ImageButton
+    val nuevoProducto = Producto(
+        nombre_producto = "peras",
+        precio = 200.00,
+        stock = 10
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu_admin)
@@ -16,5 +29,22 @@ class MenuAdminActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        guardar = findViewById(R.id.imgbStatistics)
+        guardar.setOnClickListener{
+            ref.push().setValue(nuevoProducto)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Producto guardado correctamente", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            Toast.makeText(this, "funciono", Toast.LENGTH_SHORT).show()
+        }
+
     }
+    data class Producto(
+        val nombre_producto: String = "",
+        val precio: Double = 0.0,
+        val stock: Int = 0
+    )
 }
--- app/src/main/java/com/example/minsupgest/MenuEmpleadoActivity.kt
+++ app/src/main/java/com/example/minsupgest/MenuEmpleadoActivity.kt
@@ -1,12 +1,19 @@
 package com.example.minsupgest
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MenuEmpleadoActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
