d8d6a313c0bf562d5a337bbe48b4ce5ce38a779d
ERMR1 <114266959+ermr2@users.noreply.github.com>
2025-04-09 08:08:17 -0600

    Prueba

--- app/src/main/java/com/example/minsupgest/MenuAdminActivity.kt
+++ app/src/main/java/com/example/minsupgest/MenuAdminActivity.kt
@@ -1,5 +1,6 @@
 package com.example.minsupgest
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
@@ -10,7 +11,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MenuAdminActivity : AppCompatActivity() {
-
+private  lateinit var  inventary: ImageButton
+private lateinit var  btnAgregarProducto:Button
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -21,7 +23,16 @@ class MenuAdminActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+    inventary = findViewById(R.id.imgbInventary)
+    btnAgregarProducto = findViewById(R.id.btnAgregarProducto)
+    inventary.setOnClickListener{
+        val  intent = Intent(this@MenuAdminActivity,InventarioActivity::class.java)
+        startActivity(intent)
+    }
+   btnAgregarProducto.setOnClickListener {
+       val  intent = Intent(this@MenuAdminActivity,AgregarProductoActivity::class.java)
+       startActivity(intent)
+   }
 
     }
 
