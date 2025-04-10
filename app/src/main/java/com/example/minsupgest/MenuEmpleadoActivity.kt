ab573d76137d4a4beb3628674c4b0cdcee9a6fef
OscarDs7 <a22110112@ceti.mx>
2025-04-10 17:11:44 -0600

    Eventos de navegación.

--- app/src/main/java/com/example/minsupgest/MenuEmpleadoActivity.kt
+++ app/src/main/java/com/example/minsupgest/MenuEmpleadoActivity.kt
@@ -1,7 +1,9 @@
 package com.example.minsupgest
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +15,10 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MenuEmpleadoActivity : AppCompatActivity() {
+    //Instancias
+    private lateinit var inventario: ImageButton
+    private lateinit var agregar_prod: Button
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,19 @@ class MenuEmpleadoActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //Vinculación
+        inventario = findViewById(R.id.imgbInventary2)
+        agregar_prod = findViewById(R.id.btnAgregar)
+
+        //Eventos de navegación
+        inventario.setOnClickListener {
+            val intent = Intent(this@MenuEmpleadoActivity, InventarioActivity::class.java)
+            startActivity(intent)
+        }
+        agregar_prod.setOnClickListener {
+            val intent = Intent(this@MenuEmpleadoActivity, AgregarProductoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
