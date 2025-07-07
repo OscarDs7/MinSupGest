c06aaa2143ab679341ee5fc68419d5da5a81055b
OscarDs7 <a22110112@ceti.mx>
2025-07-07 16:02:52 -0600

    Mejoras en el diseño de los activitys.

--- app/src/main/java/com/example/minsupgest/ventasActiviti.kt
+++ app/src/main/java/com/example/minsupgest/ventasActiviti.kt
@@ -1,9 +1,7 @@
 package com.example.minsupgest
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
